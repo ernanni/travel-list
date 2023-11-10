d37d4227c4d142e5285d8fa76c73decd1ee36d22
Ernanni <ernannicf@gmail.com>
2023-11-10 03:39:26 -0300

    handle toggled items and percentage stats

--- src/App.js
+++ src/App.js
@@ -12,12 +12,24 @@ function App() {
     setItems((items) => items.filter((item) => item.id !== id));
   }
 
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} onDeleteItem={handleDeleteItem} />
-      <Stats />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
--- src/components/PackingList/index.js
+++ src/components/PackingList/index.js
@@ -1,12 +1,17 @@
 import Item from '../Item';
 
-export default function PackingList({ items, onDeleteItem }) {
+export default function PackingList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
         {items &&
           items.map((item) => (
-            <Item onDeleteItem={onDeleteItem} key={item.id} item={item} />
+            <Item
+              onDeleteItem={onDeleteItem}
+              onToggleItem={onToggleItem}
+              key={item.id}
+              item={item}
+            />
           ))}
       </ul>
     </div>
